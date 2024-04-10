9f30a6c73f34a927e0477abb0c4290f0d52c7bd7
Gmicheletti <guilherme_micheletti@hotmail.com>
2024-04-09 21:46:33 -0300

    upload

--- src/main/java/com/estruturaDeDados/lista2/ListaEncadeada.java
+++ src/main/java/com/estruturaDeDados/lista2/ListaEncadeada.java
@@ -4,15 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaEncadeada {
-    public static void main(String[] args) {
 
+    public static List<Integer> setList(){
+        List<Integer> newList = new ArrayList<>();
+        return newList;
     }
 
-    public static List<Integer> createListEmpty(){
-        return new ArrayList<>();
+    public static List<Integer> addInicioLista(List<Integer> newList, int item){
+        newList.add(0,item);
+        return newList;
     }
 
-    public static List<Integer> createListEmpty(){
-        return new ArrayList<>();
+    public static List<Integer> addFimLista(List<Integer> newList, int item){
+        newList.add(item);
+        return newList;
     }
+
+    public static List<Integer> addPositionLista(List<Integer> newList, int item, int index){
+        newList.add(index,item);
+        return newList;
+    }
+
+
+    public static List<Integer> removeInicioLista(List<Integer> newList){
+        newList.remove(0);
+        return newList;
+    }
+
+    public static List<Integer> removeFimLista(List<Integer> newList){
+        int tamanho = newList.size();
+        newList.remove(tamanho -1 );
+        return newList;
+    }
+
+    public static List<Integer> removePositionLista(List<Integer> newList, int index){
+        newList.remove(index);
+        return newList;
+    }
+
+
+
 }
